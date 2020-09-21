dc489ed02825617bf7f55f6b3ab2cb0d746a6b55
Julian Caba <julian.caba85@gmail.com>
2020-09-21 11:37:08 +0200

    Añadido pinout y completado pines arduino

--- pins_arduino.h
+++ pins_arduino.h
@@ -29,59 +29,51 @@
 static const uint8_t LED_BUILTIN = 2;
 #define BUILTIN_LED  LED_BUILTIN // backward compatibility
 
-/* static const uint8_t TX = 1; */
-/* static const uint8_t RX = 3; */
+static const uint8_t TX = 1;
+static const uint8_t RX = 3;
 
-/* static const uint8_t SDA = 21; */
-/* static const uint8_t SCL = 22; */
+static const uint8_t SDA = 21;
+static const uint8_t SCL = 22;
 
-/* static const uint8_t SS    = 5; */
-/* static const uint8_t MOSI  = 23; */
-/* static const uint8_t MISO  = 19; */
-/* static const uint8_t SCK   = 18; */
+static const uint8_t SS    = 5;
+static const uint8_t MOSI  = 23;
+static const uint8_t MISO  = 19;
+static const uint8_t SCK   = 18;
 
-static const uint8_t D2 =  26;
-static const uint8_t D3 =  25;
-static const uint8_t D5 =  16;
-static const uint8_t D6 =  27;
-static const uint8_t D9 = 13;
+static const uint8_t D2  = 26;
+static const uint8_t D3  = 25;
+static const uint8_t D4  = 17;
+static const uint8_t D5  = 16;
+static const uint8_t D6  = 27;
+static const uint8_t D7  = 14;
+static const uint8_t D8  = 12;
+static const uint8_t D9  = 13;
 static const uint8_t D10 = 5;
 static const uint8_t D11 = 23;
 static const uint8_t D12 = 19;
 static const uint8_t D13 = 18;
+static const uint8_t D14 = 21;
+static const uint8_t D15 = 22;
 
 static const uint8_t A0 = 2;
 static const uint8_t A1 = 4;
-static const uint8_t A2 = 36;
+static const uint8_t A2 = 35;
+static const uint8_t A3 = 34;
+static const uint8_t A4 = 36;
+static const uint8_t A5 = 39;
 
-/* static const uint8_t A3 = 39; */
-/* static const uint8_t A4 = 32; */
-/* static const uint8_t A5 = 33; */
-/* static const uint8_t A6 = 34; */
-/* static const uint8_t A7 = 35; */
-/* static const uint8_t A10 = 4; */
-/* static const uint8_t A11 = 0; */
-/* static const uint8_t A12 = 2; */
-/* static const uint8_t A13 = 15; */
-/* static const uint8_t A14 = 13; */
-/* static const uint8_t A15 = 12; */
-/* static const uint8_t A16 = 14; */
-/* static const uint8_t A17 = 27; */
-/* static const uint8_t A18 = 25; */
-/* static const uint8_t A19 = 26; */
 
-/* static const uint8_t T0 = 4; */
-/* static const uint8_t T1 = 0; */
-/* static const uint8_t T2 = 2; */
-/* static const uint8_t T3 = 15; */
-/* static const uint8_t T4 = 13; */
-/* static const uint8_t T5 = 12; */
-/* static const uint8_t T6 = 14; */
-/* static const uint8_t T7 = 27; */
-/* static const uint8_t T8 = 33; */
-/* static const uint8_t T9 = 32; */
+static const uint8_t IO0  = 0;
+static const uint8_t IO15 = 15;
+static const uint8_t IO32 = 32;
+static const uint8_t IO33 = 33;
 
-/* static const uint8_t DAC1 = 25; */
-/* static const uint8_t DAC2 = 26; */
+
+static const uint8_t CLK = 6;
+static const uint8_t SD0 = 7;
+static const uint8_t SD1 = 8;
+static const uint8_t SD2 = 9;
+static const uint8_t SD3 = 10;
+static const uint8_t CMD = 11;
 
 #endif /* Pins_Arduino_h */
