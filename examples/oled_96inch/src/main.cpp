a3e62d278cc86582e78d893641632d53cb080482
Julian Caba <julian.caba85@gmail.com>
2020-11-15 16:45:43 +0100

    Added OLED example

---
+++ examples/oled_96inch/src/main.cpp
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void setup() {
+  Serial.begin(115200);
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+    Serial.println(F("SSD1306 allocation failed\n"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  // Clear the buffer
+  display.clearDisplay();
+
+  
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(1,0);
+  display.clearDisplay();
+  display.println("Hello World!");
+  display.display();
+
+}
+
+void loop() {
+}
